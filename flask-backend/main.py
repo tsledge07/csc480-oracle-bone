60644826994cb179421e84eafa9a558ea48271ee
sledge_te <tsledge07@gmail.com>
2020-11-04 14:09:19 -0500

    initial commit for flask back end setup

---
+++ flask-backend/main.py
@@ -0,0 +1,12 @@
+# This is our main driver for the flask back-end
+
+
+import flask
+
+app = flask.Flask("__main__")
+
+@app.route("/")
+def my_index():
+    return flask.render_template("index.html", token="Hello Flask+React")
+
+app.run(debug=True)
