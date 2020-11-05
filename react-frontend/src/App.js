0e9feb90503d846521d8a73689024f2ee1bb0e70
sledge_te <tsledge07@gmail.com>
2020-11-04 22:08:11 -0500

    having an error when trying to generate the new index.html

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+          <p>{window.token}</p>
+          <p>{window.image}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
