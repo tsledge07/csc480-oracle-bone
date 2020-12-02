0248bc1f451df8cbcf44949484a3931ed5728bee
thepacer778 <71772776+thepacer778@users.noreply.github.com>
2020-12-01 19:37:23 -0500

    Add files via upload

--- database/dbinsert.py
+++ database/dbinsert.py
@@ -5,9 +5,10 @@ def insert(id, character, gif_url):
     cursor = sqliteConnection.cursor()
     print("Connected to BoneDB")
     sqlite_insert_query = """ INSERT INTO characters (id, character, gif_url) VALUES (?, ?, ?) """
-
-    data_tuple = (id, character, gif_url)                                 
-    cursor.execute(sqlite_insert_query, data_tuple)                                                 # execute the query
+    print("Defining Tuple")
+    data_tuple = (id, character, gif_url) 
+    print("Upload to DB")
+    cursor.execute(sqlite_insert_query, data_tuple) # execute the query
     sqliteConnection.commit()
     print("Entry inserted corectly")
     cursor.close()
@@ -16,4 +17,13 @@ def insert(id, character, gif_url):
         sqliteConnection.close()
         print("The sqlite connection is closed")
 
-insert(1, "random chinese character", "randomgif.gov")
+insert(1, "邬", "https://www.hanzi5.com/assets/bishun/animation/90ac-bishun.gif")
+insert(2, "郗", "https://www.hanzi5.com/assets/bishun/animation/90d7-bishun.gif")
+insert(3, "郇", "https://www.hanzi5.com/assets/bishun/animation/90c7-bishun.gif")
+insert(4, "魏", "https://www.hanzi5.com/assets/bishun/animation/9b4f-bishun.gif")
+insert(5, "魄", "https://www.hanzi5.com/assets/bishun/animation/9b44-bishun.gif")
+insert(6, "骸", "https://www.hanzi5.com/assets/bishun/animation/9ab8-bishun.gif")
+insert(7, "髁", "https://www.hanzi5.com/assets/bishun/animation/9ac1-bishun.gif")
+insert(8, "骰", "https://www.hanzi5.com/assets/bishun/animation/9ab0-bishun.gif")
+insert(9, "髂", "https://www.hanzi5.com/assets/bishun/animation/9ac2-bishun.gif")
+insert(10, "髌", "https://www.hanzi5.com/assets/bishun/animation/9acc-bishun.gif")
